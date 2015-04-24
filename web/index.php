1be12ca8e41ab3affbbd74163ff42f86d72e406b
Guillaume Orsal <guillaume@orsal.net>
2015-04-24 12:09:30 +0200

    Password supprimés des logs

--- web/index.php
+++ web/index.php
@@ -712,6 +712,8 @@ $app->finish(function ($request, $response) use ($app) {
         $ctLog['parameters'] = $request->query->all();
     if ( is_null( $ctLog['parameters'] ) || count( $ctLog['parameters'] ) == 0 ) 
         $ctLog['parameters'] = json_decode( $request->getContent(), true );
+    if ( ! is_null( $ctLog['parameters'] && count( $ctLog['parameters'] ) ) )
+        array_walk( $ctLog['parameters'], function( &$v, $k ){ if ( in_array( $k, array( "_password", "password", "confirm-password" ), TRUE ) ) $v = ""; });
 
     $ctLog['type'] = $request->getMethod();
 
